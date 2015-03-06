dd54cb1b38f7c2469ab814e395c02acc4906c7d4
Elliott Young & Gabriela Voicu <pair+elliottayoung.gabivoicu@devbootcamp.com>
2015-03-06 15:14:24 -0600

    Write CSV parser for politicians.csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'CSV'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CSV.foreach('db/politicians.csv', headers: true, header_converters: :symbol) do |row|
+  politician = Politician.new
+  politician.hash_data = row.to_hash
+  politician.bio_id = politician.bioguide_id
+  politician.save
+end
