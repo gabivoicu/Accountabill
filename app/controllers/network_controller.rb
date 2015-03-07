af4b850e4cd298b13179d7283db252d0ef0857df
Elliott Young <elliott.a.young@gmail.com>
2015-03-07 14:10:41 -0600

    Network controller renders contribution info as JSON

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -3,6 +3,8 @@ class NetworkController < ApplicationController
   def contributions
     politician = Politician.find_by_bio_id(params[:bio_id])
 
-    @contributions = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors.json?cycle=2012&limit=50&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+    response = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors.json?cycle=2012&limit=50&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+
+    render :json => response
   end
 end
