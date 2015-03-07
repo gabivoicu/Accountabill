80fc9642cca80895cbe3873ee135759ee22e74e8
Elliott Young & Ian Agne <pair+elliottayoung.ianagne@devbootcamp.com>
2015-03-07 14:32:17 -0600

    Add method to controller to render info into JSON

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -1,4 +1,9 @@
 class NetworkController < ApplicationController
+  def politicians
+    response = HTTParty.get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=#{params[:zipcode]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+
+    render :json => response
+  end
 
   def contributions
     politician = Politician.find_by_bio_id(params[:bio_id])
