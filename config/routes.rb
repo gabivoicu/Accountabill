aa3b44f14baa7601adce3002755af981402af365
Gabriela Voicu & Ian Agne <pair+gabivoicu.ianagne@devbootcamp.com>
2015-03-06 18:41:57 -0600

    add test form to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  root 'page#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'page#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
