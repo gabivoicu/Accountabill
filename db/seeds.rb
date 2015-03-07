fd19db73622fbcca091a425694ea7a4549609f9b
Elliott Young & Gabriela Voicu <pair+elliottayoung.gabivoicu@devbootcamp.com>
2015-03-06 18:07:31 -0600

    Remove API dependency for seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,18 +15,23 @@ CSV.foreach('db/politicians.csv', headers: true, header_converters: :symbol) do 
   politician.save
 end
 
-Politician.all.each do |politician|
-  response = HTTParty.get("http://transparencydata.com/api/1.0/entities/id_lookup.json?namespace=urn%3Acrp%3Aorganization&bioguide_id=#{politician.bio_id}&apikey=API_KEY_GOES_HERE!")
-  politician.entity_id = response[0]["id"] unless response.body == "[]"
-  response = JSON.parse(response.body)
-  politician.save
-  puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
-  sleep(0.5)
+CSV.foreach('db/entity_id.csv') do |row|
+  politician = Politician.find_by(bio_id: row[0])
+  politician.entity_id = row[1]
 end
 
-CSV.open('db/entity_id.csv', 'w' ) do |writer|
-  Politician.all.each do |politician|
-    puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
-    writer << [politician.bio_id, politician.entity_id]
-  end
-end
+# Politician.all.each do |politician|
+#   response = HTTParty.get("http://transparencydata.com/api/1.0/entities/id_lookup.json?namespace=urn%3Acrp%3Aorganization&bioguide_id=#{politician.bio_id}&apikey=API_KEY_GOES_HERE!")
+#   politician.entity_id = response[0]["id"] unless response.body == "[]"
+#   response = JSON.parse(response.body)
+#   politician.save
+#   puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
+#   sleep(0.5)
+# end
+
+# CSV.open('db/entity_id.csv', 'w' ) do |writer|
+#   Politician.all.each do |politician|
+#     puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
+#     writer << [politician.bio_id, politician.entity_id]
+#   end
+# end
