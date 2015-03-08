9d72c1de5384b384239be735f7b959a3eb64d233
ianagne <ian.d.agne@gmail.com>
2015-03-08 14:08:19 -0500

    detail view hides when new search occurs

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -12,6 +12,7 @@ $(document).ready(function(){
 
         searchResultView.render()
         $("#search-container").html(searchResultView.el);
+        $('.detail').hide();
         politicians.fetch({reset: true});
     }
 
@@ -33,10 +34,9 @@ $(document).ready(function(){
     var content = $(this).html();
     console.log(content.attributes)
     
-    $(".search-results").hide();
-    
     var allDetailsView = new AllDetailsView();
     allDetailsView.render();
+    $(".search-results").hide();
     $("#results-view").html(allDetailsView.el);
   });
 });
