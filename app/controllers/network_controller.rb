3382beb41aebfa90b8346dde736638d3dbe55bfb
Elliott Young <elliott.a.young@gmail.com>
2015-03-08 09:08:19 -0500

    Add API controller call for bills

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -13,7 +13,7 @@ class NetworkController < ApplicationController
   def contributions
     politician = Politician.find_by_bio_id(params[:bio_id])
 
-    response = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors.json?cycle=2012&limit=50&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+    response = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors.json?cycle=2014&limit=50&apikey=#{ENV['SUNLIGHT_API_KEY']}")
 
     contribution_array = []
 
@@ -27,7 +27,7 @@ class NetworkController < ApplicationController
   def sectors
     politician = Politician.find_by_bio_id(params[:bio_id])
 
-    response = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors/sectors.json?cycle=2012&limit=10&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+    response = HTTParty.get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors/sectors.json?cycle=2014&limit=10&apikey=#{ENV['SUNLIGHT_API_KEY']}")
 
     sector_array = []
 
@@ -36,5 +36,54 @@ class NetworkController < ApplicationController
     end
 
     render :json => sector_array
+  end
+
+  def bills
+    response = HTTParty.get("http://congress.api.sunlightfoundation.com/bills?sponsor_id=#{params[:bio_id]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+
+    bills_array = []
+
+    response.fetch("results").each do |b_info|
+      bills_array << {official_title: b_info["official_title"]}
+    end
+
+    render :json => bills_array
+  end
+
+  private
+
+  def find_sector(sector)
+    case sector
+    when "A"
+      sector = "Agribusiness"
+    when "B"
+      sector = "Communications/Electronics"
+    when "C"
+      sector = "Construction"
+    when "D"
+      sector = "Defense"
+    when "E"
+      sector = "Energy/Natural Resources"
+    when "F"
+      sector = "Finance/Insurance/Real Estate"
+    when "H"
+      sector = "Health"
+    when "K"
+      sector = "Lawyers and Lobbyists"
+    when "M"
+      sector = "Transportation"
+    when "N"
+      sector = "Misc. Business"
+    when "Q"
+      sector = "Ideology/Single Issue"
+    when "P"
+      sector = "Labor"
+    when "W"
+      sector = "Other"
+    when "Y"
+      sector = "Unknown"
+    when "Z"
+      sector = "Administrative Use"
+    end
   end
 end
