408aba264b08c89d3316a6fc5744f9f5a58e3405
Elliott Young & Gabriela Voicu <pair+elliottayoung.gabivoicu@devbootcamp.com>
2015-03-08 16:27:09 -0500

    Able to access politician info on click

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   match '/auth/:provider', :to => "twitter#auth", :via => [:get, :post]
   match '/auth/failure', :to => "twitter#failure", :via => [:get, :post]
 
+  get '/politician/:bio_id' => "politician#details", as: "politician_details"
+  
   root 'page#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
