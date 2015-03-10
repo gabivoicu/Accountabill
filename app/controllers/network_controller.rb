35d67623842bcf5caae32dea0de1d51df7048d4d
Elliott Young <elliott.a.young@gmail.com>
2015-03-10 07:37:18 -0500

    Made consistent initializations

--- app/controllers/network_controller.rb
+++ app/controllers/network_controller.rb
@@ -1,7 +1,7 @@
 class NetworkController < ApplicationController
   def politicians
-    response = get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=#{params[:zipcode]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
     politician_array = []
+    response = get("http://congress.api.sunlightfoundation.com/legislators/locate?zip=#{params[:zipcode]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
     response.fetch("results").each do |p_info|
       politician_array << find(p_info["bioguide_id"]).hash_data
     end
@@ -9,8 +9,8 @@ class NetworkController < ApplicationController
   end
 
   def politician_names
-    name = split_into_names(params[:name])
     politician_array = []
+    name = split_into_names(params[:name])
     response = get("http://congress.api.sunlightfoundation.com/legislators?first_name=#{name[0]}&last_name=#{name[1]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
     response.parsed_response.fetch("results").each do |p_info|
       politician_array << find(p_info["bioguide_id"]).hash_data
@@ -23,9 +23,9 @@ class NetworkController < ApplicationController
   end
 
   def contributions
+    contribution_array = []
     politician = find(params[:bio_id])
     response = get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors.json?cycle=2014&limit=50&apikey=#{ENV['SUNLIGHT_API_KEY']}")
-    contribution_array = []
     response.parsed_response.each do |c_info|
       contribution_array << {total_amount: c_info["total_amount"].to_i, name: c_info["name"]}
     end
@@ -33,9 +33,9 @@ class NetworkController < ApplicationController
   end
 
   def sectors
+    sector_array = []
     politician = find(params[:bio_id])
     response = get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors/sectors.json?cycle=2014&limit=10&apikey=#{ENV['SUNLIGHT_API_KEY']}")
-    sector_array = []
     response.parsed_response.each do |s_info|
       sector_array << {sector: find_sector(s_info["sector"]), count: s_info["count"], amount: s_info["amount"]}
     end
@@ -43,9 +43,9 @@ class NetworkController < ApplicationController
   end
 
   def industries
+    industries_array = []
     politician = find(params[:bio_id])
     response = get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors/industries.json?cycle=2014&limit=10&apikey=#{ENV['SUNLIGHT_API_KEY']}")
-    industries_array = []
     response.parsed_response.each do |ind_info|
       industries_array << {name: capitalize_all_words!(ind_info["name"]), count: ind_info["count"], amount: ind_info["amount"]}
     end
@@ -54,8 +54,8 @@ class NetworkController < ApplicationController
 
 
   def bills
-    response = get("http://congress.api.sunlightfoundation.com/bills?sponsor_id=#{params[:bio_id]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
     bills_array = []
+    response = get("http://congress.api.sunlightfoundation.com/bills?sponsor_id=#{params[:bio_id]}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
     response.fetch("results").each do |b_info|
       bills_array << {bill_id: b_info["bill_id"], official_title: b_info["official_title"], open_congress: b_info.fetch("urls")["opencongress"]}
     end
@@ -63,9 +63,9 @@ class NetworkController < ApplicationController
   end
 
   def contributor_types
+    contributor_type_array = []
     politician = find(params[:bio_id])
     response = get("http://transparencydata.com/api/1.0/aggregates/pol/#{politician.entity_id}/contributors/type_breakdown.json?cycle=2012&apikey=#{ENV['SUNLIGHT_API_KEY']}")
-    contributor_type_array = []
     contributor_type_array.push(title: "Individuals", count: response.parsed_response.fetch("Individuals").at(0), total_amount: response.parsed_response.fetch("Individuals").at(1))
     contributor_type_array.push(title: "PACs", count: response.parsed_response.fetch("PACs").at(0), total_amount: response.parsed_response.fetch("PACs").at(1))
     render :json => contributor_type_array
