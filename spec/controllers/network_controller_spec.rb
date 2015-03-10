aa3386b0b34c52ba3ad13741dfb42f854072d2d8
Elliott Young <elliott.a.young@gmail.com>
2015-03-10 11:40:25 -0500

    More Tests

--- spec/controllers/network_controller_spec.rb
+++ spec/controllers/network_controller_spec.rb
@@ -27,6 +27,18 @@ describe NetworkController do
         expect(JSON.parse(response.body).length).to be >= (3)
       end
     end
+
+    it "should return data" do
+      VCR.use_cassette("politicians") do
+        zipcode = 60302
+
+        response = get :politicians, zipcode: zipcode
+        body = JSON.parse(response.body)
+        politician = body.first
+        expect(politician["firstname"]).to eq("Mark")
+        expect(politician["lastname"]).to eq("Kirk")
+      end
+    end
   end
 
   context "Finding a Politician by Name" do
@@ -36,6 +48,18 @@ describe NetworkController do
 
         response = get :politician_names, name: name
         expect { JSON.parse(response.body) }.to_not raise_error 
+      end
+    end
+
+    it "should return data" do
+      VCR.use_cassette("politician_names") do
+        name = "Kirk"
+
+        response = get :politician_names, name: name
+        body = JSON.parse(response.body)
+        politician = body.first
+        expect(politician["firstname"]).to eq("Mark")
+        expect(politician["lastname"]).to eq("Kirk")
       end
     end
   end
@@ -60,6 +84,19 @@ describe NetworkController do
         expect(contributions[0]['total_amount']).to be_kind_of(Integer)
       end
     end
+
+    it "should return data" do
+      VCR.use_cassette("contributions") do
+        bio_id = "K000360"
+
+        response = get :contributions, bio_id: bio_id
+        body = JSON.parse(response.body)
+        contribution = body.first
+
+        expect(contribution["total_amount"]).to eq(52000)
+        expect(contribution["name"]).to eq("Highfields Capital Management")
+      end
+    end
   end
 
   context "Finding Contributions by Sector" do
@@ -69,6 +106,45 @@ describe NetworkController do
 
         response = get :sectors, bio_id: bio_id
         expect { JSON.parse(response.body) }.to_not raise_error 
+      end
+    end
+
+    it "should translate the sector codes to real sectors" do
+      VCR.use_cassette("sectors") do
+        bio_id = "K000360"
+
+        response = get :sectors, bio_id: bio_id
+        body = JSON.parse(response.body)
+        sector = body.first
+      
+        expect(sector["sector"]).to eq("Finance/Insurance/Real Estate")
+      end
+    end
+
+    it "should convert count and amount to integers" do
+      VCR.use_cassette("sectors") do
+        bio_id = "K000360"
+
+        response = get :sectors, bio_id: bio_id
+        body = JSON.parse(response.body)
+        sector = body.first
+
+        expect(sector["count"]).to be_kind_of(Integer)
+        expect(sector["total_amount"]).to be_kind_of(Integer)       
+      end
+    end
+
+    it "should return data" do
+      VCR.use_cassette("sectors") do
+        bio_id = "K000360"
+
+        response = get :sectors, bio_id: bio_id
+        body = JSON.parse(response.body)
+        sector = body.first
+
+        expect(sector["sector"]).to eq("Finance/Insurance/Real Estate")
+        expect(sector["count"]).to eq(373)
+        expect(sector["total_amount"]).to eq(606950)
       end
     end
   end
