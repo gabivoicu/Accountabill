b27c78df636c4ddf9168071bef776bbe89b3a85e
Elliott Young <elliott.a.young@gmail.com>
2015-03-10 17:24:29 -0500

    Fix seed file, term dates now in CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,29 +27,29 @@ CSV.foreach('db/entity_id.csv') do |row|
   politician.save
 end
 
-Politician.all.each do |politician|
-  puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
-  if politician.entity_id
-    response = HTTParty.get("http://congress.api.sunlightfoundation.com/legislators?bioguide_id=#{politician.bio_id}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
+# Politician.all.each do |politician|
+#   puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
+#   if politician.entity_id
+#     response = HTTParty.get("http://congress.api.sunlightfoundation.com/legislators?bioguide_id=#{politician.bio_id}&apikey=#{ENV['SUNLIGHT_API_KEY']}")
 
-    puts "Parsed Response:"
-    puts response.parsed_response.fetch("results")
+#     puts "Parsed Response:"
+#     puts response.parsed_response.fetch("results")
 
-    if response.parsed_response.fetch("results")[0] != nil
+#     if response.parsed_response.fetch("results")[0] != nil
 
-      politician.term_start = response.parsed_response.fetch("results")[0]["term_start"]
-      politician.term_end = response.parsed_response.fetch("results")[0]["term_end"]
-      politician.save
+#       politician.term_start = response.parsed_response.fetch("results")[0]["term_start"]
+#       politician.term_end = response.parsed_response.fetch("results")[0]["term_end"]
+#       politician.save
 
-      sleep(0.5)
-    end
-  end
-  puts ""
-end
-
-# CSV.open('db/entity_id.csv', 'w' ) do |writer|
-#   Politician.all.each do |politician|
-#     puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
-#     writer << [politician.bio_id, politician.entity_id, politician.bio, politician.photo_url, politician.term_start, politician.term_end]
+#       sleep(0.5)
+#     end
 #   end
+#   puts ""
 # end
+
+CSV.open('db/entity_id.csv', 'w' ) do |writer|
+  Politician.all.each do |politician|
+    puts "#{politician.firstname} #{politician.lastname} - #{politician.bio_id}, #{politician.entity_id}"
+    writer << [politician.bio_id, politician.entity_id, politician.bio, politician.photo_url, politician.term_start, politician.term_end]
+  end
+end
