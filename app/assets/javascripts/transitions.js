b023a8b835e210ba13526d6151d2964b01281816
Ian Agne <ian.d.agne@gmail.com>
2015-03-10 08:36:45 -0500

    remove twitter oauth from body and hide #results-view by default

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -1,12 +1,14 @@
 var Transition = ({
   searchToDetail: function(){
     $("#search-container").hide();
+    $("#results-view").show();
     $("#front-page-header").css("margin-top", "1%");
     $(document).foundation('tab', 'reflow');
   },
   resetToDefault: function(){
     $('.detail').hide();
     $(".search-results").hide();
+    $("#results-view").hide();
     $("#front-page-header").css("margin-top", "10%");
   },
   defaultToDisplaySearchResults: function(){
