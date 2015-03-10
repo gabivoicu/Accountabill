aa999d3b76e5722a9e488376de8f6f822ab9a94d
Elliott Young <elliott.a.young@gmail.com>
2015-03-10 15:06:27 -0500

    Remove other env twitter keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.twitter_key = ENV['TWITTER_API_KEY']
-  config.twitter_secret = ENV['TWITTER_SECRET_KEY']
 end
