64e6cb76a22e90c586e4551eec40866e56d6b371
Gabriela Voicu <gabrielavoicu16@gmail.com>
2015-03-10 14:27:09 -0500

    /query/60630 not working yet

--- app/assets/javascripts/search_results.js
+++ app/assets/javascripts/search_results.js
@@ -1,28 +1,28 @@
 function renderResults(searchVal) {
 
-if (isNaN(searchVal)) {
+  if (isNaN(searchVal)) {
 
-      // var name = $("#zip-search-form input").val();
+    var politicians = new PoliticiansCollection({ name: searchVal, entryType: "String" });
 
-      var politicians = new PoliticiansCollection({ name: searchVal, entryType: "String" });
+    var searchResultView = new PoliticianSearchResultsView({collection: politicians});
 
-      var searchResultView = new PoliticianSearchResultsView({collection: politicians});
+    searchResultView.render();
+    $("#search-container").html(searchResultView.el);
+    politicians.fetch({reset: true});
+    Transition.defaultToDisplaySearchResults();
+  }
 
-      searchResultView.render();
-      $("#search-container").html(searchResultView.el);
-      politicians.fetch({reset: true});
-      Transition.defaultToDisplaySearchResults();
-    }
-    else{
-      // var zipcode = $("#zip-search-form input").val();
+  else{
 
-      var politicians = new PoliticiansCollection({ zipcode: searchVal, entryType: "Integer" });
+    var politicians = new PoliticiansCollection({ zipcode: searchVal, entryType: "Integer" });
 
-      var searchResultView = new PoliticianSearchResultsView({collection: politicians});
+    var searchResultView = new PoliticianSearchResultsView({collection: politicians});
 
-      searchResultView.render();
-      $("#search-container").html(searchResultView.el);
-      politicians.fetch({reset: true});
-      Transition.defaultToDisplaySearchResults();
-    }
+    searchResultView.render();
+
+    $("#search-container").html(searchResultView.el);
+    politicians.fetch({reset: true});
+    Transition.defaultToDisplaySearchResults();
+  }
+
 }
