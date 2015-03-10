c82d6d07a0bdc2351b37c8e7879a375c60e567df
Elliott Young <elliott.a.young@gmail.com>
2015-03-09 19:16:16 -0500

    Add additional views and fix current user bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  helper_method :current_user
+  # helper_method :current_user
 
-  private
+  # private
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
  def self.from_omniauth(auth)
+    
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
