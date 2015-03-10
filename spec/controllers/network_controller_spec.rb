2132b6c5753a932bb8b39bc27c43d6f6451fc117
Elliott Young <elliott.a.young@gmail.com>
2015-03-10 08:22:57 -0500

    Add skeleton for Network Controller Tests

--- spec/controllers/network_controller_spec.rb
+++ spec/controllers/network_controller_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
 
-
 describe NetworkController do
-  it "converts total amount donated to an integer" do
-    VCR.use_cassette("contributions") do
-      bio_id = "D000607"
-      politician = Politician.create!(bio_id: bio_id, entity_id: "4d86ffd297c84c059577c0afe4f8d0ee")
-
-      response = get :contributions, bio_id: bio_id
-      contributions = JSON.parse(response.body)
-
-      expect(contributions[0]['total_amount']).to be_kind_of(Integer)
-    end
+  context "Finding a Politician by Zip Code" do
   end
 
+  context "Finding a Politician by Name" do
+  end
+
+  context "Finding Contributions to a Politician" do
+  end
+
+  context "Finding Contributions by Sector" do
+  end
+
+  context "Finding Contribuions by Industry" do
+  end
+
+  context "Finding Sponsored Bills" do
+  end
+
+  context "Finding Types of Contributors" do
+  end
 end
+
+
+# it "converts total amount donated to an integer" do
+#   VCR.use_cassette("contributions") do
+#     bio_id = "D000607"
+#     politician = Politician.create!(bio_id: bio_id, entity_id: "4d86ffd297c84c059577c0afe4f8d0ee")
+
+#     response = get :contributions, bio_id: bio_id
+#     contributions = JSON.parse(response.body)
+
+#     expect(contributions[0]['total_amount']).to be_kind_of(Integer)
+#   end
+# end
