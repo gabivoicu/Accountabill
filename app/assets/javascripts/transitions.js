13db282b61b05fe120fc764713cd683df36815c9
Ian Agne <ian.d.agne@gmail.com>
2015-03-11 10:15:09 -0500

    move header and tagline away from explanation of search box

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -14,6 +14,6 @@ var Transition = ({
   defaultToDisplaySearchResults: function(){
     $('#results-view').hide();
     $("#search-container").show();
-    $("#front-page-header").css("margin-top", "1%");
+    $("#front-page-header").css("margin", "1%");
   }
 })
