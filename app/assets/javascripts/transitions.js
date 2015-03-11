535c885025edf9e92b51ede4dea2b0ae5fe2c3ee
Ian Agne <ian.d.agne@gmail.com>
2015-03-11 10:21:04 -0500

    reduce all margin vs margin-top on transition to detail view

--- app/assets/javascripts/transitions.js
+++ app/assets/javascripts/transitions.js
@@ -2,7 +2,7 @@ var Transition = ({
   searchToDetail: function(){
     $("#search-container").hide();
     $("#results-view").show();
-    $("#front-page-header").css("margin-top", "1%");
+    $("#front-page-header").css("margin", "1%");
     $(document).foundation('tab', 'reflow');
     $(document).foundation();
   },
